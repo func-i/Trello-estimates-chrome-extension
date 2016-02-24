7b6cf9417b2d00e2fb394795c0ed47a297ae84bf
tfchang <tingfchang@gmail.com>
2016-02-24 14:33:59 -0500

    build process for Chrome extension

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,14 +1,30 @@
 module.exports = function(grunt) {
 
   // Load the plugins
-  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-coffee');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
 
 
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+
+    // clean /dist folder
+    clean: ["dist"],
+
+    // copy html, image and vendor files to /dist
+    copy: {
+      main: {
+        files: [
+          { expand: true, cwd: 'src/html', src: '**', dest: 'dist/html/' },
+          { expand: true, cwd: 'src/image', src: '**', dest: 'dist/image/' },
+          { expand: true, cwd: 'src/vendor', src: '**', dest: 'dist/vendor/' }
+        ]
+      }
+    }, // copy
 
     // concatenate CSS files
     concat: {
@@ -17,8 +33,7 @@ module.exports = function(grunt) {
       },
       dist: {
         src: [
-          'src/stylesheet/vendor/ui-lightness/jquery-ui-1.10.3.custom.css',
-          'src/stylesheet/vendor/ui-lightness/jquery-ui-1.10.3.custom.min.css',
+          'src/stylesheet/vendor/jquery-ui-1.10.3.custom.min.css',
           'src/stylesheet/vendor/bootstrap.min.css',
           'src/stylesheet/board.css',
           'src/stylesheet/card.css',
@@ -50,8 +65,10 @@ module.exports = function(grunt) {
 
   // Grunt tasks
   grunt.registerTask('build', [
+    'clean',
+    'copy',
     'concat',
-    'coffee:compileJoin'
+    'coffee'
   ]);
 
 };
